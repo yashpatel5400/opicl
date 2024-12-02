2eb95e02d7783e7605849d2fef74993a6d8e1eb4
yashpatel5400 <ypatel5400@gmail.com>
2024-12-02 15:06:39 -0500

    Check in starter code

---
+++ darcy.py
@@ -0,0 +1,103 @@
+import torch
+import matplotlib.pyplot as plt
+import sys
+from torch.optim import AdamW
+
+from neuralop.models import FNO
+from neuralop import Trainer
+from neuralop.datasets import load_darcy_flow_small
+from neuralop.utils import count_model_params
+from neuralop import LpLoss, H1Loss
+
+device = 'cpu'
+
+train_loader, test_loaders, data_processor = load_darcy_flow_small(
+        n_train=1000, batch_size=32, 
+        test_resolutions=[16, 32], n_tests=[100, 50],
+        test_batch_sizes=[32, 32],
+)
+data_processor = data_processor.to(device)
+
+model = FNO(n_modes=(16, 16),
+             in_channels=3, 
+             out_channels=1,
+             hidden_channels=32, 
+             projection_channel_ratio=2)
+model = model.to(device)
+
+n_params = count_model_params(model)
+print(f'\nOur model has {n_params} parameters.')
+sys.stdout.flush()
+
+optimizer = AdamW(model.parameters(), 
+                                lr=8e-3, 
+                                weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30)
+
+l2loss = LpLoss(d=2, p=2)
+h1loss = H1Loss(d=2)
+
+train_loss = h1loss
+eval_losses={'h1': h1loss, 'l2': l2loss}
+
+print('\n### MODEL ###\n', model)
+print('\n### OPTIMIZER ###\n', optimizer)
+print('\n### SCHEDULER ###\n', scheduler)
+print('\n### LOSSES ###')
+print(f'\n * Train: {train_loss}')
+print(f'\n * Test: {eval_losses}')
+sys.stdout.flush()
+
+trainer = Trainer(model=model, n_epochs=20,
+                  device=device,
+                  data_processor=data_processor,
+                  wandb_log=False,
+                  use_distributed=False,
+                  verbose=True)
+
+trainer.train(train_loader=train_loader,
+              test_loaders=test_loaders,
+              optimizer=optimizer,
+              scheduler=scheduler, 
+              regularizer=False, 
+              training_loss=train_loss,
+              eval_losses=eval_losses)
+
+test_samples = test_loaders[16].dataset
+
+fig = plt.figure(figsize=(7, 7))
+for index in range(3):
+    data = test_samples[index]
+    data = data_processor.preprocess(data, batched=False)
+    # Input x
+    x = data['x']
+    # Ground-truth
+    y = data['y']
+    # Model prediction
+    out = model(x.unsqueeze(0))
+
+    ax = fig.add_subplot(3, 3, index*3 + 1)
+    ax.imshow(x[0], cmap='gray')
+    if index == 0: 
+        ax.set_title('Input x')
+    plt.xticks([], [])
+    plt.yticks([], [])
+
+    ax = fig.add_subplot(3, 3, index*3 + 2)
+    ax.imshow(y.squeeze())
+    if index == 0: 
+        ax.set_title('Ground-truth y')
+    plt.xticks([], [])
+    plt.yticks([], [])
+
+    ax = fig.add_subplot(3, 3, index*3 + 3)
+    ax.imshow(out.squeeze().detach().numpy())
+    if index == 0: 
+        ax.set_title('Model prediction')
+    plt.xticks([], [])
+    plt.yticks([], [])
+
+fig.suptitle('Inputs, ground-truth output and prediction (16x16).', y=0.98)
+plt.tight_layout()
+plt.savefig("result.png")
+fig.show()
